b1327cf1599785a3d2d9b646e552e9615b52832c
cjs177 <cjs177@ca.rr.com>
2020-12-18 01:06:32 -0800

    initial commit

---
+++ library.js
@@ -0,0 +1,12 @@
+let library = [];
+
+function book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    
+}
